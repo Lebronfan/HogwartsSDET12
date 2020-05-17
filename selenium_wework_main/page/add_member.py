fc24fda0312c4c4870507937bcdb52c7a278e0a2
ruotongyu <yuruotong1@163.com>
2020-05-17 16:45:42 +0800

    base_page的初步封装

--- selenium_wework_main/page/add_member.py
+++ selenium_wework_main/page/add_member.py
@@ -1,23 +1,23 @@
 from time import sleep
 
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
+
+from selenium_wework_main.page.base_page import BasePage
 
 
-class AddMember:
-    def __init__(self, driver: WebDriver):
-        self._driver = driver
+class AddMember(BasePage):
 
     def add_member(self):
         # sendkeys
         sleep(2)
-        self._driver.find_element(By.ID, 'username').send_keys("abcdefffff")
-        self._driver.find_element(By.ID, 'memberAdd_acctid').send_keys("abcdefffff")
-        self._driver.find_element(By.ID, 'memberAdd_phone').send_keys("11111111111")
-        self._driver.find_element(By.CSS_SELECTOR, '.js_btn_save').click()
+        self.find(By.ID, 'username').send_keys("abcdefffff")
+        self.find(By.ID, 'memberAdd_acctid').send_keys("abcdefffff")
+        self.find(By.ID, 'memberAdd_acctid').send_keys("abcdefffff")
+        self.find(By.ID, 'memberAdd_phone').send_keys("11111111111")
+        self.find(By.CSS_SELECTOR, '.js_btn_save').click()
         sleep(5)
         return True
 
     def get_member(self):
-        elements = self._driver.find_elements(By.CSS_SELECTOR, '.member_colRight_memberTable_td:nth-child(2)')
+        elements = self.finds(By.CSS_SELECTOR, '.member_colRight_memberTable_td:nth-child(2)')
         return [element.get_attribute("title") for element in elements]
--- selenium_wework_main/page/main.py
+++ selenium_wework_main/page/main.py
@@ -1,18 +1,18 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from time import sleep
+
 from selenium.webdriver.common.by import By
 
 from selenium_wework_main.page.add_member import AddMember
+from selenium_wework_main.page.base_page import BasePage
 
 
-class Main:
-    def __init__(self):
-        options = Options()
-        options.debugger_address="127.0.0.1:9222"
-        self._driver = webdriver.Chrome(options=options)
-        self._driver.get('https://work.weixin.qq.com/wework_admin/frame')
+class Main(BasePage):
+    _base_url = 'https://work.weixin.qq.com/wework_admin/frame'
 
     def goto_add_member(self):
         # click add member
-        self._driver.find_element(By.CSS_SELECTOR, '.index_service_cnt_itemWrap:nth-child(1)').click()
+        # self.find(By.CSS_SELECTOR, '.index_service_cnt_itemWrap:nth-child(1)').click()
+        self.find(By.ID, 'menu_contacts').click()
+        sleep(2)
+        self.find(By.CSS_SELECTOR, '.js_has_member>div:nth-child(1)>a:nth-child(2)').click()
         return AddMember(self._driver)
